b5ad71e573dff4a1e3565711e84a5f73beb0d0ac
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-24 02:21:07 +0000

    Done.

--- ruby practice.rb
+++ ruby practice.rb
@@ -1,60 +1,47 @@
+
 def create_an_empty_array
 
-  []
-end
+[]
 
+end
+p create_an_empty_array()
 
 def create_an_array
 
   ["soccer", "football", "basketball", "tennis"]
 end
-
+puts create_an_array()
 
 def add_element_to_end_of_array(array, element)
 
   array = ["wow", "I", "am", "really", "learning"]
-  array.push("arrays!")
+  array.push("hdgfhsd")
 end
+p add_element_to_end_of_array(nil,nil)
 
-def add_element_to_start_of_array(array, element)
-
-  array = ["I", "am", "really", "learning"]
-  array.unshift("wow")
-end
 
 def remove_element_from_end_of_array(array)
 
   array = ["I", "am", "really", "learning", "arrays!"]
   array.pop()
-  
+  return array
 end
+p remove_element_from_end_of_array(nil)
 
-def remove_element_from_start_of_array(array)
-
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.shift()
-end
 
 def retrieve_element_from_index(array, index_number)
 
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[2]
+  array
 end
+p retrieve_element_from_index(nil,nil)
 
-def retrieve_first_element_from_array(array)
-
-   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-   array[0]
-end
-
-def retrieve_last_element_from_array(array)
-
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[5]
-end
 
 def update_element_from_index(array, index_number, element)
 
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[4] = "totally"
+  array
 end
+p update_element_from_index(nil,nil,nil)
