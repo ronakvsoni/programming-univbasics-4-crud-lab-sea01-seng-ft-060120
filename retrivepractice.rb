f745a1487704962de73111dd7f72a065ffa965d4
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-25 03:52:21 +0000

    Automatically backed up by Learn

---
+++ retrivepractice.rb
@@ -0,0 +1,22 @@
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ 
+
+ 
+
+
+def retrieve_element_from_index(array, index_number)
+  
+
+
+  array[2]
+  array
+end
+p retrieve_element_from_index(array,nil)
+
+
+def retrieve_first_element_from_array(array)
+
+  
+   array[3]
+end
+p retrieve_first_element_from_array(array)
